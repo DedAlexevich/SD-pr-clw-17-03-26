8ab29ccb72ca5935c4e3e48bf5b82a159a1094f7
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-03-17 10:44:14 +0300

    Declare vector

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,11 +1,12 @@
-#include <iostream>
-
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+#include <cstddef>
 template< class T >
 struct Vector {
   T* data;
   size_t size, cap;
 };
 
-int main()
-{}
+#endif
+
 
