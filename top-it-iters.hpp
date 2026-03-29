023e7cb6e2cb266accf585d1895e2d84050ea60f
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-03-29 17:32:11 +0300

    Define methods of const iterator

--- top-it-iters.hpp
+++ top-it-iters.hpp
@@ -8,20 +8,20 @@ namespace kuznetsov {
     explicit CIter(T* ptr):
       ptr_(ptr)
     {}
-    const T& operator*();
+    const T& operator*() const;
     CIter& operator++();
     CIter& operator--();
     CIter& operator+=(size_t n);
     CIter& operator-=(size_t n);
-    CIter operator+(size_t n);
-    CIter operator-(size_t n);
-    size_t operator-(const CIter&);
-    bool operator==(const CIter&);
-    bool operator!=(const CIter&);
-    bool operator>(const CIter&);
-    bool operator>=(const CIter&);
-    bool operator<(const CIter&);
-    bool operator<=(const CIter&);
+    CIter operator+(size_t n) const noexcept;
+    CIter operator-(size_t n) const noexcept;
+    int operator-(const CIter&) const noexcept;
+    bool operator==(const CIter&) const noexcept;
+    bool operator!=(const CIter&) const noexcept;
+    bool operator>(const CIter&) const noexcept;
+    bool operator>=(const CIter&) const noexcept;
+    bool operator<(const CIter&) const noexcept;
+    bool operator<=(const CIter&) const noexcept;
   private:
     T* ptr_;
   };
@@ -50,7 +50,93 @@ namespace kuznetsov {
   };
 }
 
+template< class T >
+const T& kuznetsov::CIter<T>::operator*() const
+{
+  return *ptr_;
+}
 
+template< class T >
+kuznetsov::CIter<T>& kuznetsov::CIter<T>::operator++()
+{
+  ++ptr_;
+  return *this;
+}
+
+template< class T >
+kuznetsov::CIter<T>& kuznetsov::CIter<T>::operator--()
+{
+  --ptr_;
+  return *this;
+}
+
+template< class T >
+kuznetsov::CIter<T>& kuznetsov::CIter<T>::operator+=(size_t n)
+{
+  ptr_ += n;
+  return *this;
+}
+
+template< class T >
+kuznetsov::CIter<T>& kuznetsov::CIter<T>::operator-=(size_t n)
+{
+  ptr_ -= n;
+  return *this;
+}
+
+template< class T >
+kuznetsov::CIter<T> kuznetsov::CIter<T>::operator+(size_t n) const noexcept
+{
+  return CIter< T >{ptr_ + n};
+}
+
+template< class T >
+kuznetsov::CIter<T> kuznetsov::CIter<T>::operator-(size_t n) const noexcept
+{
+  return CIter< T >{ptr_ - n};
+}
+
+template< class T >
+int kuznetsov::CIter<T>::operator-(const CIter& rhs) const noexcept
+{
+  return this->ptr_ - rhs.ptr_;
+}
+
+template< class T >
+bool kuznetsov::CIter<T>::operator==(const CIter& rhs) const noexcept
+{
+  return this->ptr_ == rhs.ptr_;
+}
+
+template< class T >
+bool kuznetsov::CIter<T>::operator!=(const CIter& rhs) const noexcept
+{
+  return !(*this == rhs);
+}
+
+template< class T >
+bool kuznetsov::CIter<T>::operator>(const CIter& rhs) const noexcept
+{
+  return this->ptr_ > rhs.ptr_;
+}
+
+template< class T >
+bool kuznetsov::CIter<T>::operator>=(const CIter& rhs) const noexcept
+{
+  return this->ptr_ >= rhs.ptr_;
+}
+
+template< class T >
+bool kuznetsov::CIter<T>::operator<(const CIter& rhs) const noexcept
+{
+  return this->ptr_ < rhs.ptr_;
+}
+
+template< class T >
+bool kuznetsov::CIter<T>::operator<=(const CIter& rhs) const noexcept
+{
+  return this->ptr_ <= rhs.ptr_;
+}
 
 #endif
 
